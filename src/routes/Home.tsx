14c38f58c97d2202be4e726dfc2983efabd6da99
whoiseon <whois__@naver.com>
2023-02-09 20:04:25 +0900

    using: Apollo/client useQuery

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,32 @@
+import { gql, useApolloClient, useQuery } from '@apollo/client';
+import { useEffect, useState } from 'react';
+
+const ALL_MOVIES = gql`
+  query getMovies {
+    allMovies {
+      id
+      title
+    }
+  }
+`;
+
 function Home() {
-  return <div>This is a list of movie</div>;
+  const { data, loading, error } = useQuery(ALL_MOVIES);
+  console.log(data);
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <h1>Could not fetch :(</h1>;
+  }
+  return (
+    <ul>
+      <h1>Movies</h1>
+      {data.allMovies.map((movie: any) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default Home;
